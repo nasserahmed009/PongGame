a56da6f86e8e328fbe2da6921bb0f3280d92cb10
nasser <anasserabdrabo@gmail.com>
2019-06-18 19:14:34 +0200

    adding levels to the game

--- tennisGame.js
+++ tennisGame.js
@@ -1,15 +1,18 @@
 let canvas = document.getElementById('canvas');
 let canvasContext = canvas.getContext('2d');
 
-let playGame = false;
+let pauseGame = true;
+let showingScreen = false; // if levelup or game over screens are showed
 
 let playGroundColor = 'black';
 let ballRadius = 10;
 let ballX = canvas.width / 2;
 let ballY = canvas.height / 2;
-let dx = document.getElementById('ballSpeed').value; //horizontal speed of the ball
-let dy = document.getElementById('ballSpeed').value; // vertical speed of the ball
+let dx = parseInt(document.getElementById('ballSpeed').value); //horizontal speed of the ball
+let dy = parseInt(document.getElementById('ballSpeed').value); // vertical speed of the ball
 let framesPerSecond = 60;
+
+let gameLevel = 1;
 
 // paddles variables
 let paddleHeight = 100;
@@ -20,11 +23,12 @@ let paddle2Y = 100;
 let player1Name = document.getElementById('player1Name').value;
 let player1Score = 0;
 let player2Score = 0;
+let finalScore = 3;
 
 function startPauseGame(event){
   event.preventDefault();
-  playGame = !playGame;
-  if(playGame){
+  pauseGame = !pauseGame;
+  if(!pauseGame){
     event.target.text = "Pause";
     event.target.style.background = "#ff4444";
   }else{
@@ -44,7 +48,7 @@ function changePlayGroundColor(event, color){
 }
 
 function changeBallSpeed(){
-  resetGame();
+  resetPositionsAndSpeeds();
   dx = dy = document.getElementById('ballSpeed').value;
 }
 
@@ -74,28 +78,35 @@ function drawPaddles(){
   canvasContext.fillRect(canvas.width - paddleWidth ,paddle2Y,paddleWidth, paddleHeight);
 }
 
-function drawScores(){
-  canvasContext.fillText(player1Name + " =", 50, 50);
-  canvasContext.fillText(player1Score, 50, 70);
-  canvasContext.fillText("Player2 score = ", canvas.width-130, 50);
-  canvasContext.fillText(player2Score, canvas.width-130, 70);
+function drawScoresAndLevel(){
+  canvasContext.fillStyle = "white";
+  canvasContext.font = "10px PressStart2P";
+  canvasContext.fillText(player1Name + " = " + player1Score, 50, 50);
+  canvasContext.fillText("Player2 = "+ player2Score, canvas.width-160, 50);
+
+  // print level
+  let levelText = "LEVEL " + gameLevel;
+  canvasContext.fillStyle = playGroundColor;
+  canvasContext.fillRect((canvas.width/2) - (canvasContext.measureText(levelText).width/2) - 10 ,30,canvasContext.measureText(levelText).width+20, 35);
+  canvasContext.fillStyle = '#fff';
+  canvasContext.fillText(levelText, (canvas.width/2) - (canvasContext.measureText(levelText).width/2) , 50);
 }
 
 function updateNames(){
   player1Name = document.getElementById('player1Name').value;
 }
 //reset the postition of the ball to the center of the playGround
-function resetGame(){
+function resetPositionsAndSpeeds(){
   ballX = canvas.width/2;
   ballY = canvas.height/2;
-  paddleHeight= 100;
   paddle1Y = canvas.height/2 - paddleHeight/2;
   paddle2Y = canvas.height/2 - paddleHeight/2;
+  dy = parseInt(document.getElementById('ballSpeed').value);
   dx=-dx;
 }
 
 function increaseDifficulty(){
-  if(paddleHeight>30) paddleHeight--;
+  if(paddleHeight>30) paddleHeight-=2;
 
   //increase the speed of the ball
   if(dx<0) dx--;
@@ -104,13 +115,57 @@ function increaseDifficulty(){
   else dy++;
 }
 
+
+function showLevelUpWindow(){
+  drawPlayground(playGroundColor);
+  canvasContext.fillStyle = "white";
+  canvasContext.font = "10px PressStart2P";
+  canvasContext.fillText("LEVEL UP", (canvas.width/2)-((canvasContext.measureText("LEVEL UP").width)/2), canvas.height/2);
+  canvasContext.fillText("click to level up", (canvas.width/2)-((canvasContext.measureText("click to level up").width)/2), canvas.height/2 + 50);
+}
+
+function shhowGameOverWindow(){
+  drawPlayground(playGroundColor);
+  canvasContext.fillStyle = "white";
+  canvasContext.font = "10px PressStart2P";
+  canvasContext.fillText("GAME OVER", (canvas.width/2)-((canvasContext.measureText("GAME OVER").width)/2), canvas.height/2);
+  canvasContext.fillText("click to start new game", (canvas.width/2)-((canvasContext.measureText("click to start new game").width)/2), canvas.height/2+20);
+}
+
+function checkWinner(){
+  if(player1Score>finalScore){
+    showLevelUpWindow();
+    showingScreen = true;
+    return true;
+  }else if(player2Score>finalScore){
+    shhowGameOverWindow();
+    showingScreen = true;
+    return true;
+  }else{
+    showingScreen = false;
+    return false;
+  }
+}
+
+function levelUp(){
+  gameLevel++;
+  player1Score  = 0;
+  player2Score = 0;
+  increaseDifficulty();
+  showingScreen = false;
+}
+
+function resetGame(){
+  resetPositionsAndSpeeds();
+  gameLevel = 1;
+  player1Score = 0;
+  player2Score = 0;
+  showingScreen = false;
+}
+
 function getMousePosition(event){
   let playGround = canvas.getBoundingClientRect();
   let root = document.documentElement;
-
-  console.log(event.y);
-  console.log(playGround.top);
-  console.log(root.scrollTop);
 
   return {
     x: event.x - playGround.left,
@@ -119,8 +174,8 @@ function getMousePosition(event){
 }
 
 function computerPaddlePlay(){
-  if(paddle2Y+paddleHeight/2 < ballY-45) paddle2Y+=6;
-  else if(paddle2Y+paddleHeight/2 > ballY+45) paddle2Y-=6;
+  if(paddle2Y+paddleHeight/2 < ballY-30) paddle2Y+=9.5;
+  else if(paddle2Y+paddleHeight/2 > ballY+30) paddle2Y-=9.5;
 }
 
 function moveBall(){
@@ -129,14 +184,20 @@ function moveBall(){
 
   // ball postition for player1
   if(ballX < 0 + ballRadius){
-    if(ballY>paddle1Y && ballY<paddle1Y+paddleHeight)dx=-dx;
+    if(ballY>paddle1Y && ballY<paddle1Y+paddleHeight){
+      dx=-dx;
+
+      let deltaY = ballY - (paddle1Y+paddleHeight)/2;
+      dy = deltaY*0.06;
+    }
     else{
       player2Score++;
-      playGame = false;
+      checkWinner();
+      pauseGame = true;
       //adding delay to the game when a player scores
       setTimeout(function(){
-        resetGame();
-        playGame = true;
+        resetPositionsAndSpeeds();
+        pauseGame = false;
       },1000);
     }
   }
@@ -144,14 +205,20 @@ function moveBall(){
 
   //ball postition for player2
   if(ballX > canvas.width - ballRadius){
-    if(ballY>paddle2Y && ballY<paddle2Y+paddleHeight) dx=-dx;
+    if(ballY>paddle2Y && ballY<paddle2Y+paddleHeight){
+       dx=-dx;
+
+       let deltaY = ballY - (paddle2Y+paddleHeight)/2;
+       dy = deltaY*0.05;
+     }
     else {
       player1Score++;
-      playGame = false;
+      checkWinner();
+      pauseGame = true;
       //adding delay to the game when a player scores
       setTimeout(function(){
-        resetGame();
-        playGame = true;
+        resetPositionsAndSpeeds();
+        pauseGame = false;
       },1000);
     }
   }
@@ -163,22 +230,28 @@ function moveBall(){
 }
 
 canvas.addEventListener('mousemove', function(event){
-  if(!playGame) return;
+  if(pauseGame) return;
   let mousePos = getMousePosition(event);
-  console.log(mousePos);
-
   //moving paddle1 with the movement of mouse
   paddle1Y = mousePos.y - paddleHeight/2; //to move the paddle from its center
-
 });
 
+canvas.addEventListener('click', function(){
+  if(!showingScreen) return;
+  if(player1Score>player2Score) levelUp();
+  else resetGame();
+});
+
+
+
 setInterval(function(){
+  if(showingScreen) return;
   drawPlayground(playGroundColor);
   drawNet();
-  drawScores();
+  drawScoresAndLevel();
   drawPaddles();
   drawTennisBall();
-  if(!playGame) return;
+  if(pauseGame) return;
   computerPaddlePlay();
   moveBall();
 }, 1000/framesPerSecond);
